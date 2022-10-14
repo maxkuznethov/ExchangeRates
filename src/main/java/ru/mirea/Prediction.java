eb980e3a0913a22c389712fa60e0cf6973d1be0a
Maxim Kuznethov <max.kuznethov03@gmail.com>
2022-10-14 20:30:41 +0300

    add calculate avg

--- src/main/java/ru/mirea/Prediction.java
+++ src/main/java/ru/mirea/Prediction.java
@@ -1,4 +1,14 @@
 package ru.mirea;
 
+import java.util.List;
+
 public class Prediction {
+
+    public static double calculateAvg(List<Double> list){
+        double result = 0;
+        for (int i = 1; i <list.size() ; i++) {
+            result+= list.get(i)-list.get(i-1);
+        }
+        return result/(list.size()-1);
+    }
 }
