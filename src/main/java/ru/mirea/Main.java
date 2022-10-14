5585ad18f590c6195c0aa300f601c1d8cd46ffa8
Maxim Kuznethov <max.kuznethov03@gmail.com>
2022-10-14 21:47:30 +0300

    add main method code

--- src/main/java/ru/mirea/Main.java
+++ src/main/java/ru/mirea/Main.java
@@ -1,7 +1,38 @@
 package ru.mirea;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите валюты");
+        String cur1 = scanner.nextLine();
+        String cur2 = scanner.nextLine();
+        LocalDate endDate = LocalDate.now();
+        LocalDate startDate = endDate.minusDays(6);
+        String request = String.format("https://api.apilayer.com/currency_data/timeframe?apikey=DlfFoDe9TUUhzkH7eSL6OTXSQuDsNlDY&start_date=%s&end_date=%s&currencies=%s&source=%s",
+                startDate, endDate, cur1, cur2);
+        Map<String, Double> currencyRate = Currency.getCurrencyRate(Currency.parseAPI(request), cur2, cur1);
+        if (currencyRate != null) {
+            Map<String, Double> sortedCurrency= new TreeMap<>(currencyRate);
+            List<Double> currencyRateList = new ArrayList<>();
+            System.out.println("История курса:");
+            sortedCurrency.forEach((k, v) -> {
+                System.out.println(k + " : " + v);
+                currencyRateList.add(v);
+            });
+
+            System.out.println("Прогноз будущего курса:");
+            List<Double> predictionList = Prediction.calculatePrediction(currencyRateList, 3);
+            for (int i = 0; i < predictionList.size(); i++) {
+                System.out.println(endDate.plusDays(i+1)+" : "+predictionList.get(i));
+            }
+
+        }
+        else {
+            System.out.println("Некоректные данные");
+        }
     }
 }
