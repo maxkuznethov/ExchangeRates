d0293a8db21b624c3605f25f03b5c5bf36523acc
Maxim Kuznethov <max.kuznethov03@gmail.com>
2022-10-14 16:54:58 +0300

    add json converter

--- src/main/java/ru/mirea/Converter.java
+++ src/main/java/ru/mirea/Converter.java
@@ -1,10 +1,15 @@
 package ru.mirea;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.HashMap;
 import java.util.Map;
 
 public class Converter {
 
     public static Map<String, Object> toMap(String str){
-        return null;
+        return new Gson().fromJson(str, new TypeToken<HashMap<String, Object>>() {
+        }.getType());
     }
 }
