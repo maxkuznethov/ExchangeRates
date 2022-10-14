5ab5dee2e671c091397bc86d311e2b72f491d26d
Maxim Kuznethov <max.kuznethov03@gmail.com>
2022-10-14 20:57:33 +0300

    add calculate prediction

--- src/main/java/ru/mirea/Prediction.java
+++ src/main/java/ru/mirea/Prediction.java
@@ -1,14 +1,25 @@
 package ru.mirea;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Prediction {
 
-    public static double calculateAvg(List<Double> list){
+    protected static double calculateAvg(List<Double> list){
         double result = 0;
         for (int i = 1; i <list.size() ; i++) {
             result+= list.get(i)-list.get(i-1);
         }
         return result/(list.size()-1);
+    }
+
+    public static List<Double> calculatePrediction(List<Double> currencyRate, int days){
+        double avgChange = calculateAvg(currencyRate);
+        List<Double> result = new ArrayList<>();
+        double lastRate = currencyRate.get(currencyRate.size()-1);
+        for (int i = 1; i <= days; i++) {
+            result.add(lastRate+avgChange*i);
+        }
+        return result;
     }
 }
