fef5c8368a62c58a0fe30a1b106ecad8928c17e9
Maxim Kuznethov <max.kuznethov03@gmail.com>
2022-10-14 17:37:10 +0300

    refactor parse api

--- src/main/java/ru/mirea/Currency.java
+++ src/main/java/ru/mirea/Currency.java
@@ -10,18 +10,14 @@ import java.util.Map;
 
 public class Currency {
 
-    public static Map<String,Object> parseAPI(String request) throws IOException {
-
+    public static Map<String, Object> parseAPI(String request) throws IOException {
         StringBuilder result = new StringBuilder();
-        URL url = new URL(request);
-        URLConnection connection = url.openConnection();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String line;
-        while ((line = reader.readLine()) != null)
-            result.append(line);
-        reader.close();
-        String stringResult = result.toString();
-        Map<String, Object> resultMap = Converter.toMap(stringResult);
-        return resultMap;
+        URLConnection connection = new URL(request).openConnection();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+            while (reader.ready()) {
+                result.append(reader.readLine());
+            }
+        }
+        return Converter.toMap(result.toString());
     }
 }
