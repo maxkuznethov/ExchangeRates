0845b6ee3f0e4153395fa258d0add92506ce74c0
Maxim Kuznethov <max.kuznethov03@gmail.com>
2022-10-14 17:24:03 +0300

    add parse api

--- src/main/java/ru/mirea/Currency.java
+++ src/main/java/ru/mirea/Currency.java
@@ -1,11 +1,27 @@
 package ru.mirea;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Map;
 
 public class Currency {
 
-    public static Map<String,Object> parseAPI(String request){
+    public static Map<String,Object> parseAPI(String request) throws IOException {
 
-        return null;
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(request);
+        URLConnection connection = url.openConnection();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String line;
+        while ((line = reader.readLine()) != null)
+            result.append(line);
+        reader.close();
+        String stringResult = result.toString();
+        Map<String, Object> resultMap = Converter.toMap(stringResult);
+        return resultMap;
     }
 }
