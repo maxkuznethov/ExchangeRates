fb1d116bd739977f1f82f2afb08e8649869781d5
Maxim Kuznethov <max.kuznethov03@gmail.com>
2022-10-14 18:45:50 +0300

    add get currency test

--- src/test/java/ru/mirea/TestCurrency.java
+++ src/test/java/ru/mirea/TestCurrency.java
@@ -3,23 +3,33 @@ package ru.mirea;
 import junit.framework.TestCase;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class TestCurrency extends TestCase {
-    private static final String TEST_REQUEST = "https://api.apilayer.com/currency_data/timeframe?start_date=2016-02-25&end_date=2017-02-21&currencies=USD&source=RUB";
+    private static final String TEST_REQUEST = "https://api.apilayer.com/currency_data/historical?apikey=DlfFoDe9TUUhzkH7eSL6OTXSQuDsNlDY&date=2018-02-22&currencies=EUR,GBP,JPY&source=USD";
 
     @Test
     public void testJsonConverter(){
-        Map<String, Object> map = new HashMap<String, Object>();
+        Map<String, Object> map = new HashMap<>();
         map.put("123", 123.0);
         String json = "{\"123\":123}";
         assertEquals(map, Converter.toMap(json));
     }
 
     @Test
-    public void testParseAPI() {
+    public void testParseAPI() throws IOException {
         assertTrue((Boolean) Currency.parseAPI(TEST_REQUEST).get("success"));
+    }
+
+    @Test
+    public void testGetCurrencyRate(){
+        String json = "{\"success\": true, \"quotes\": {\"2016-02-25\": {\"RUBUSD\": 0.013228}}}";
+        Map<String, Object> map = new HashMap<>();
+        map.put("2016-02-25", 0.013228);
+        assertEquals(map,Currency.getCurrencyRate(Converter.toMap(json)));
+
     }
 
 }
